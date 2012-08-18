72c1e94eb5164d7bbfed8d1cc488f12ed9544eb1
Janosch Woschitz <janosch.woschitz@gmail.com>
2012-08-18 15:54:23 +0200

    added model for tweet, configured object mapping.

---
+++ TwitterClient/TCTweet.h
@@ -0,0 +1,13 @@
+//
+//  TCTweet.h
+//  TwitterClient
+//
+//  Created by Janosch Woschitz on 8/18/12.
+//  Copyright (c) 2012 Janosch Woschitz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCTweet : NSObject
+
+@end
---
+++ TwitterClient/TCTweet.m
@@ -0,0 +1,13 @@
+//
+//  TCTweet.m
+//  TwitterClient
+//
+//  Created by Janosch Woschitz on 8/18/12.
+//  Copyright (c) 2012 Janosch Woschitz. All rights reserved.
+//
+
+#import "TCTweet.h"
+
+@implementation TCTweet
+
+@end
